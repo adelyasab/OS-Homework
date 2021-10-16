9bd45337f20c61a74a03dc30195666ee3cd8c467
adelya <55406007+adelyasab@users.noreply.github.com>
2021-10-16 22:26:29 +0300

    Create parent.py

---
+++ parent/parent.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import os
+import random
+import sys
+
+child = 1
+n = int(sys.argv[1])
+for i in range(n):
+	if child == 0:
+		break
+	child = os.fork()
+if child > 0 :
+	for i in range(0, n):
+		ret = os.wait()
+		print(f"Child process {ret[0]} finished. Code:{ret[1]}")
+else:
+	os.execl('child.py', "child.py", str(random.choice(range(5, 11))))
