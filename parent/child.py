7ac664a709be4cb81fa65e1d964b374969f36463
adelya <55406007+adelyasab@users.noreply.github.com>
2021-10-16 22:35:08 +0300

    Update child.py

--- parent/child.py
+++ parent/child.py
@@ -8,3 +8,5 @@ print("Child process started. Pid ", os.getpid(), " with argument  ", sys.argv[1
 time.sleep(int(sys.argv[1]))
 print("Pid of ChP: ", os.getpid(), " finished")
 exit(random.choice([0, 1]))
+
+#Сабирзянова Аделя 11-901
