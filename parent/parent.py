d31a6a3cdf82fbe9f9123652a26f8f56ac482beb
adelya <55406007+adelyasab@users.noreply.github.com>
2021-10-16 22:31:07 +0300

    Update parent.py

--- parent/parent.py
+++ parent/parent.py
@@ -12,6 +12,6 @@ for i in range(n):
 if child > 0 :
 	for i in range(0, n):
 		ret = os.wait()
-		print(f"Child process {ret[0]} finished. Code:{ret[1]}")
+		print("Child process ", ret[0], "finished. Code: ", ret[1])
 else:
 	os.execl('child.py', "child.py", str(random.choice(range(5, 11))))
